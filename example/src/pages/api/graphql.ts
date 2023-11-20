542ead539cb50eebfc56b654adb5c21f0fbd277e
ahonn <ahonn95@outlook.com>
2023-11-20 22:02:42 +0800

    feat: add nextjs api maxDuration

--- example/src/pages/api/graphql.ts
+++ example/src/pages/api/graphql.ts
@@ -1,6 +1,8 @@
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { createContext, createApolloServer } from 'spore-graphql';
 
+export const maxDuration = 300;
+
 export default startServerAndCreateNextHandler(
   createApolloServer({
     introspection: true,
