fe72ed0874e3209fad7b8fcdac37134d1363a4f3
ahonn <ahonn95@outlook.com>
2024-01-19 18:12:09 +1100

    fix: fix example endpoint

--- example/src/pages/index.tsx
+++ example/src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function EmbeddedSandbox() {
   return (
     <main>
       <ApolloSandbox
-        initialEndpoint={`https://spore-graphql.vercel.app/api/graphql`}
+        initialEndpoint={`/api/graphql`}
       />
     </main>
   );
